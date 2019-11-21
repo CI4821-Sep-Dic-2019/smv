465994ee15d740637871ada2887652f86ba94adc
rubmary <rubmaryrojas@gmail.com>
2019-11-21 17:07:24 -0400

    Inicializar servidor de nombre (#2)

--- apps/name_server/test/sn_test.exs
+++ apps/name_server/test/sn_test.exs
@@ -1,8 +1,23 @@
 defmodule SNTest do
-  use ExUnit.Case
-  doctest SN
+    use ExUnit.Case
+    doctest SN
 
-  test "greets the world" do
-    assert SN.hello() == :world
-  end
+    setup do
+        central_server = start_supervised!(SN)
+        %{central_server: central_server}
+    end
+
+    test "greets the world" do
+        assert SN.hello() == :world
+    end
+
+    test "Get central server", %{central_server: central_server} do
+        assert SN.get_address(central_server) == :ok
+    end
+
+    test "Set and Get central server", %{central_server: central_server} do
+        address = :"m1@gus-Aspire-A515-51"
+        SN.set_address(central_server, address)
+        assert SN.get_address(central_server) == address
+    end
 end
