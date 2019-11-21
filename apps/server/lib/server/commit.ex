2456e53c4bd32fff8d52671e8f21f9e758edd32f
Rubmary Rojas <rubmaryrojas@gmail.com>
2019-11-21 00:02:48 -0400

    Add commits functions

--- apps/server/lib/server/commit.ex
+++ apps/server/lib/server/commit.ex
@@ -7,7 +7,7 @@ defmodule Server.Commit do
     defstruct filename: "", timestamp: :os.system_time(:millisecond), message: ""
 
     @doc """
-    Starts a new agent with two maps: ({filename, timestamp} -> [node]) and ({filename, timestamp} -> message)
+    Starts a new agent with two maps: ({filename, timestamp} -> [node]) and (filename ->  [{timestamp,message}])
     """
     def start_link(_opts \\ []) do
         Agent.start_link(fn -> {%{}, %{}} end)
@@ -16,68 +16,106 @@ defmodule Server.Commit do
     @doc """
     Gets a list of nodes of `{filename, timestamp}`.
     """
-    def get_nodes(dict, {filename, timestamp}) 
+    def get_nodes(commits, {filename, timestamp})
     when is_integer(timestamp) and is_binary(filename) do
-        Agent.get(dict, &Map.get(elem(&1, 0), {filename, timestamp}))
+        Agent.get(commits, &Map.get(elem(&1, 0), {filename, timestamp}))
     end
 
     @doc """
-    Gets message of commit with `{filename, timestamp}`.
+    Gets timestamps and messages of commits with `filename`.
     """
-    def get_message(dict, {filename, timestamp})
-    when is_integer(timestamp) and is_binary(filename) do
-        Agent.get(dict, &Map.get(elem(&1, 1), {filename, timestamp}))
+    def get_filename_commits(commits, filename)
+    when is_binary(filename) do
+        Agent.get(commits, &Map.get(elem(&1, 0), filename))
     end
 
     @doc """
     Add `machine` as a node where commit is stored and its `message`.
     """
-    def add(dict, %Server.Commit{filename: filename, timestamp: timestamp, message: message}, machine) 
+    def add(commits, %Server.Commit{filename: filename, timestamp: timestamp, message: message}, machine)
     when is_atom(machine) and is_integer(timestamp) and is_binary(filename) and is_binary(message) do
-
-        Agent.update(dict, fn {nodes_map, msg_map} -> 
+        Agent.update(commits, fn {nodes_map, commits_msg} ->
             {
                 Map.update(nodes_map, {filename, timestamp}, [machine], &[machine | &1]),
-                Map.put(msg_map, {filename, timestamp}, message)
+                Map.update(
+                    commits_msg,
+                    filename,
+                    [{timestamp, message}],
+                    &insert_timestamp_message(&1, timestamp, message)
+                )
             }
         end)
+    end
 
+    @doc """
+    Add several machines where commit is stored and its `message`.
+    """
+    def add(commits, %Server.Commit{filename: filename, timestamp: timestamp, message: message}, machines)
+    when is_integer(timestamp) and is_binary(filename) and is_binary(message) do
+        Agent.update(commits, fn {nodes_map, commits_msg} ->
+            {
+                Map.update(nodes_map, {filename, timestamp}, machines, fn list -> machines++list end),
+                Map.update(
+                    commits_msg,
+                    filename,
+                    [{timestamp, message}],
+                    &insert_timestamp_message(&1, timestamp, message)
+                )
+            }
+        end)
     end
 
     @doc """
     Gets nodes where latest commit for `filename` is stored.
     """
-    def get_latest_nodes(dict, filename) do
-        Agent.get(dict, fn {map, _} ->
-            Stream.filter(map, fn {{name, _}, _} -> name == filename end )
-            |> Enum.reduce({{:error, -1}, []}, &compare_commits(&1, &2))
-        end)
-        |> elem(1)
+    def get_nodes_latest_commits(commits, filename)
+    when is_binary(filename) do
+        commit = get_latest_commit(commits, filename)
+        get_nodes(commits, {commit.filename, commit.timestamp})
     end
 
     @doc """
     Gets latest commit for `filename`
     """
-    # TODO: Optimizar la obtención del timestamp mayor y evitar el reduce O(n). Quizás agregando otro mapa filename -> timestamp
-    def get_latest_commit(dict, filename) do
-        {{filename, timestamp}, message} = Agent.get(dict, fn {_, map} ->
-            Stream.filter(map, fn {{name, _}, _} -> name == filename end )
-            |> Enum.reduce({{:error, -1}, []}, &compare_commits(&1, &2) )
-        end)
-        %Server.Commit {
-            filename: filename,
-            timestamp: timestamp,
-            message: message
-        }
+    def get_latest_commit(commits, filename)
+    when is_binary(filename) do
+        case get_filename_commits(commits, filename) do
+            [{timestamp, message} | _ ] -> %Server.Commit {
+                filename: filename,
+                timestamp: timestamp,
+                message: message
+            }
+            [] -> %Server.Commit {
+                filename: filename,
+                timestamp: -1,
+                message: :error
+            }
+        end
     end
 
-    #TODO: Obtener commits ordenados por timestamp
-    def get_commits(dict, filename) do
-        nil
+    @doc """
+    Gets latest `n` commits for `filename`
+    """
+    def get_latest_commits(commits, filename, n)
+    when is_binary(filename) and is_integer(n) do
+        commits_inf = Enum.take(get_filename_commits(commits, filename), n)
+        Enum.map(commits_inf, fn timestamp, message ->
+                %Server.Commit{
+                filename: filename,
+                timestamp: timestamp,
+                message: message
+            } end
+        )
     end
 
-    defp compare_commits({{_, c1}, _} = x, {{_, c2}, _} = y) do
-        if (c1 <= c2), do: y, else: x 
+    defp insert_timestamp_message(list, timestamp, message) do
+        case list do
+            [] -> [{timestamp, message}]
+            [{t0, m0} | tail] -> if t0 < timestamp do
+                [{timestamp, message} | list]
+            else
+                [{t0, m0} | insert_timestamp_message(tail, timestamp, message)]
+            end
+        end
     end
-
 end
--- apps/server/test/commits_test.exs
+++ apps/server/test/commits_test.exs
@@ -2,29 +2,29 @@ defmodule ServerTest.Commit do
     use ExUnit.Case, async: true
 
     setup do
-        dict = start_supervised!(Server.Commit)
+        commits = start_supervised!(Server.Commit)
         m1 = :foo@computer
         m2 = :bar@computer
         m3 = :baz@matrix
-        %{dict: dict, machines: [m1, m2, m3]}
+        %{commits: commits, machines: [m1, m2, m3]}
     end
 
-    test "add and fetch commits' info", %{dict: dict, machines: [m1, m2, m3]} do
+    test "add and fetch commits' info", %{commits: commits, machines: [m1, m2, m3]} do
         commit = %Server.Commit{
-            filename: "test_files/file.c", 
-            timestamp: 124, 
-            message: "test"
+            filename: "file 1",
+            timestamp: 1,
+            message: "test 1"
         }
 
-        Server.Commit.add(dict, commit, m1)
-        Server.Commit.add(dict, commit, m2)
-        Server.Commit.add(dict, %{commit | :timestamp => 125}, m3)
+        Server.Commit.add(commits, commit, m1)
+        Server.Commit.add(commits, commit, m2)
+        Server.Commit.add(commits, %{commit | :timestamp => 125}, m3)
 
-        assert Server.Commit.get_nodes(dict, {commit.filename, commit.timestamp}) == [m2, m1]
-        assert Server.Commit.get_latest_nodes(dict, commit.filename) == [m3]
+        assert Server.Commit.get_nodes(commits, {commit.filename, commit.timestamp}) == [m2, m1]
+        assert Server.Commit.get_latest_nodes(commits, commit.filename) == [m3]
 
-        assert Server.Commit.get_message(dict, {commit.filename, commit.timestamp}) == commit.message
+        assert Server.Commit.get_message(commits, {commit.filename, commit.timestamp}) == commit.message
 
-        assert Server.Commit.get_latest_commit(dict, commit.filename) == %{commit | :timestamp => 125}
+        assert Server.Commit.get_latest_commit(commits, commit.filename) == %{commit | :timestamp => 125}
     end
 end
