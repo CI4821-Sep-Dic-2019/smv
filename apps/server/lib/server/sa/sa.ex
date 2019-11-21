7952623c78a68b3601215a5dcc604ff7b7c31da0
Rubmary Rojas <rubmaryrojas@gmail.com>
2019-11-21 18:06:28 -0400

    Improve sa

--- apps/server/lib/server/sa/sa.ex
+++ apps/server/lib/server/sa/sa.ex
@@ -1,27 +1,29 @@
 defmodule SA do
-  @moduledoc """
-  Storage Server or "Servidor de Almacenamiento".
-  """
+    @moduledoc """
+    Storage Server or "Servidor de Almacenamiento".
+    """
 
-  @doc """
-  Gets the content of a file's commit.
-  """
-  def get_file({filename, commit}) do
-    File.read (get_name {filename, commit})
-  end
+    @doc """
+    Gets the content of a file's commit.
+    """
+    def get_file(commit) do
+        File.read("#{path()}#{get_name(commit)}")
+    end
 
-  @doc """
-  Creates a file with name `filename`, `commit` and `content`.
-  """
-  def put_file({filename, commit}, content) do
-    {:ok, file} = File.open(get_name({filename, commit}), [:write])
-    IO.binwrite(file, content)
-    File.close(file)
-  end
+    @doc """
+    Creates a file with name `filename`, `commit` and `content`.
+    """
+    def put_file(commit, content) do
+        {:ok, file} = File.open("#{path()}#{get_name(commit)}", [:write])
+        IO.binwrite(file, content)
+        File.close(file)
+    end
 
-  def get_name({filename, commit}) do
-    "#{filename}-#{inspect commit}"
-  end
+    def get_name(%Server.Commit{filename: filename, timestamp: timestamp, message: _}) do
+        "#{filename}-#{timestamp}"
+    end
 
+    defp path() do
+        "../../files/"
+    end
 end
-  
