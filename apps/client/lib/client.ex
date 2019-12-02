55391c1a925c87263e06218629ab25bac801de55
Rubmary Rojas <rubmaryrojas@gmail.com>
2019-12-01 21:40:36 -0400

    Fix client error

--- apps/client/lib/client.ex
+++ apps/client/lib/client.ex
@@ -163,42 +163,28 @@ defmodule Client do
         Task.await(task)
     end
 
-    defp get_central_server(dns)
-    when is_atom(dns) do
-        {:ok, server} = File.read(path_file())
-        central_server = String.to_atom(server)
-        if test_comunication(central_server) == :error do
-            task = Task.Supervisor.async(
+    defp get_central_server(dns, tries \\ 3)
+    when is_atom(dns) and is_integer(tries) do
+        task = Task.Supervisor.async(
                 {Client.CoordTasks, dns},
                 SN,
                 :get_address,
                 []
             )
-            central_server = Task.await(task)
-            with {:ok, file} <- File.open(path_file(), [:write]) do
-                IO.write(file, ":\"#{central_server}\"")
-                File.close(file)
+        central_server = Task.await(task)
+        if Node.ping(central_server) == :pang do
+            case tries do
+                0 ->
+                    IO.puts("No se pudo conectar con el servidor central")
+                    Process.exit(self(), :kill)
+                n ->
+                    Process.sleep((4-n)*1000)
+                    get_central_server(dns, n-1)
             end
+        else
             central_server
         end
     end
-
-    defp test_comunication(central_server)
-    when is_atom(central_server) do
-        try do
-            task = Task.Supervisor.async(
-                {Client.CoordTasks, central_server},
-                SC,
-                :test_comunication,
-                []
-            )
-            Task.await(task)
-        catch
-            :exit, _ -> :error
-        end
-    end
-
-    defp path_file do 'apps/client/server.txt' end
 
     defp dns do :"dns@rubmary-Inspiron-7370" end
 
